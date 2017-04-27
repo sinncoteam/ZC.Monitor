e4cea5aa62e17b0026948b5174417aedf8ed0720
sinnco <tianofxuan@163.com>
2017-04-27 23:37:03 +0800

    add multi series

--- ZC.Client/MainWindow.xaml.cs
+++ ZC.Client/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ViCore.Logging;
 using Visifire.Charts;
 using ZC.Client.Base;
 using ZC.Utils;
@@ -45,92 +47,125 @@ namespace ZC.Client
 
         void ExportFiles()
         {
-            List<ExcelObject> objList = new List<ExcelObject>();
-            for (int i = 1;i<=3; i++)
+            while (true)
             {
-                string path = filepath + i;
-                if (Directory.Exists(path))
+                List<ExcelObject> objList = new List<ExcelObject>();
+                for (int i = 1; i <= 31; i++)
                 {
-                    
-                    //数据统计_2017年4月.xlsx
-                    string filename = string.Format("\\数据统计_{0}年{1}月.xlsx", DateTime.Now.Year, DateTime.Now.Month);
-                    path += filename;
-                    showText("开始读取文件" + path);
-                    ExcelHelper exc = new ExcelHelper(path);
-                    var dt = exc.ExcelToDataTable("sheet1", false,5);
-                    foreach(DataRow row in dt.Rows)
+                    string path = filepath + i;
+                    if (Directory.Exists(path))
                     {
-                       
-                        string machineId = row[(int)ExcelCols.检测机号].ToString();
-                        var item = objList.Where(a => a.MachineId == machineId).FirstOrDefault();
-                        if (item != null)
+                        //数据统计_2017年4月.xlsx
+                        try
                         {
-                            item.TotalCheck += Convert.ToInt32(row[(int)ExcelCols.检验总数]);
-                            item.TotalGood += Convert.ToInt32(row[(int)ExcelCols.总合格数]);
-                            item.TotalBad += Convert.ToInt32(row[(int)ExcelCols.总不良数]);
-                            double good = Math.Round( (double)item.TotalGood * 100 / item.TotalCheck, 2);
-                            double bad = Math.Round((double)item.TotalBad * 100 / item.TotalCheck, 2);
-                            item.TotalGoodPercent = good;
-                            item.TotalBadPercent = bad;
+                            path = ReadFile(objList, path);
                         }
-                        else
+                        catch(Exception ex)
                         {
-                            ExcelObject obj = new ExcelObject();
-                            obj.MachineId = row[(int)ExcelCols.检测机号].ToString();
-                            obj.TotalCheck = Convert.ToInt32(row[(int)ExcelCols.检验总数]);
-                            obj.TotalGood = Convert.ToInt32(row[(int)ExcelCols.总合格数]);
-                            obj.TotalGoodPercent = StringHelper.PercentToInt(row[(int)ExcelCols.总合格率].ToString());
-                            obj.TotalBad = Convert.ToInt32(row[(int)ExcelCols.总不良数]);
-                            obj.TotalBadPercent = StringHelper.PercentToInt(row[(int)ExcelCols.总不良率].ToString());
-                            objList.Add(obj);
+                            Logging4net.WriteError(ex, "读取文件失败！");
                         }
                     }
-                    //int n = 0;
-                    //foreach(DataColumn col in dt.Columns)
-                    //{
-                    //    if (colList.Count > n)
-                    //    {
-                    //        var dcitem = colList[n];
-                    //        col.ColumnName = dcitem;
-                    //    }
-                    //    else
-                    //    {
-                    //        break;
-                    //    }
-                    //    n++;
-                    //}
-                    //dg_result.AutoGenerateColumns = true;
-                    //dg_result.ItemsSource = dt.DefaultView;
+                }
+                int total = 0;
+                int totalgood = 0;
+
+                Dictionary<string, Dictionary<string, double>> dict = new Dictionary<string, Dictionary<string, double>>();
+
+                Dictionary<string, double> mDict = new Dictionary<string, double>();
+                Dictionary<string, double> nDict = new Dictionary<string, double>();
+                foreach (var item in objList)
+                {
+                    mDict.Add(item.MachineId + "号机", item.TotalGoodPercent);
+                    nDict.Add(item.MachineId + "号机", item.TotalBadPercent);
+                    total += item.TotalCheck;
+                    totalgood += item.TotalGood;
+                }
+                dict.Add("合格率", mDict);
+                dict.Add("不合格率", nDict);
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    CreateChartColumn("总体检测情况图", dict, "%");
+                }));
+                double totalgoodper = Math.Round((double)totalgood * 100 / total, 2);
+                showValue(total.ToString(), totalgoodper + "%");
+                Thread.Sleep(pauseSeconds * 1000);
+            }
+        }
+
+        private string ReadFile(List<ExcelObject> objList, string path)
+        {
+            string filename = string.Format("\\数据统计_{0}年{1}月.xlsx", DateTime.Now.Year, DateTime.Now.Month);
+            path += filename;
+            showText("开始读取文件" + path);
+            ExcelHelper exc = new ExcelHelper(path);
+            var dt = exc.ExcelToDataTable("sheet1", false, 5);
+            foreach (DataRow row in dt.Rows)
+            {
+
+                string machineId = row[(int)ExcelCols.检测机号].ToString();
+                var item = objList.Where(a => a.MachineId == machineId).FirstOrDefault();
+                if (item != null)
+                {
+                    item.TotalCheck += Convert.ToInt32(row[(int)ExcelCols.检验总数]);
+                    item.TotalGood += Convert.ToInt32(row[(int)ExcelCols.总合格数]);
+                    item.TotalBad += Convert.ToInt32(row[(int)ExcelCols.总不良数]);
+                    double good = Math.Round((double)item.TotalGood * 100 / item.TotalCheck, 2);
+                    double bad = Math.Round((double)item.TotalBad * 100 / item.TotalCheck, 2);
+                    item.TotalGoodPercent = good;
+                    item.TotalBadPercent = bad;
+                }
+                else
+                {
+                    ExcelObject obj = new ExcelObject();
+                    obj.MachineId = row[(int)ExcelCols.检测机号].ToString();
+                    obj.TotalCheck = Convert.ToInt32(row[(int)ExcelCols.检验总数]);
+                    obj.TotalGood = Convert.ToInt32(row[(int)ExcelCols.总合格数]);
+                    obj.TotalGoodPercent = StringHelper.PercentToInt(row[(int)ExcelCols.总合格率].ToString());
+                    obj.TotalBad = Convert.ToInt32(row[(int)ExcelCols.总不良数]);
+                    obj.TotalBadPercent = StringHelper.PercentToInt(row[(int)ExcelCols.总不良率].ToString());
+                    objList.Add(obj);
                 }
             }
-            int total = 0;
-            int totalgood = 0;
-            List<string> machineList = new List<string>();
-            List<double> resultList = new List<double>();
-            foreach(var item in objList)
-            {
-                machineList.Add(item.MachineId + "号机合格率");
-                resultList.Add(item.TotalGoodPercent);
-                machineList.Add(item.MachineId + "号机不合格率");
-                resultList.Add(item.TotalBadPercent);
-                total += item.TotalCheck;
-                totalgood += item.TotalGood;                
-            }
-            
-            CreateChartColumn("总体检测情况图", machineList, resultList, "%");
 
-            double totalgoodper = Math.Round((double)totalgood * 100 / total, 2);
-            tb_total_yes.Text = totalgood.ToString();
-            tb_total_yesper.Text = totalgoodper.ToString() + "%";
+            return path;
         }
+
         void showText(string text)
         {
-            this.rtb_show.AppendText(text + "\r");
-            this.rtb_show.ScrollToEnd();
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.rtb_show.AppendText(text + "\r");
+                this.rtb_show.ScrollToEnd();
+            }));            
         }
+        void showValue(string t1, string t2)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                tb_total_yes.Text = t1;
+                tb_total_yesper.Text = t2;
+            }));
+        }
+        int pauseSeconds = 5;
+        bool pauseAble = true;
         private void btn_start_Click(object sender, RoutedEventArgs e)
         {
-            ExportFiles();
+            btn_start.IsEnabled = false;
+
+            try
+            {
+                pauseSeconds = Convert.ToInt32(tb_time.Text);
+            }
+            catch { }
+            if (pauseAble)
+            {                
+                ThreadStart ts = new ThreadStart(ExportFiles);
+                Thread th = new Thread(ts);
+                //th.SetApartmentState(ApartmentState.STA);
+                th.Start();
+            }
+            pauseAble = false;
+            btn_start.IsEnabled = true;
         }
 
         public static List<string> colList = new List<string>();
@@ -159,6 +194,68 @@ namespace ZC.Client
             colList.Add("占检验数比例");
         }
 
+        public void CreateChartColumn(string name, Dictionary<string,  Dictionary<string, double>> dict, string suffix)
+        {
+            Chart chart = new Chart();
+            //设置图标的宽度和高度
+            chart.Width = 800;
+            chart.Height = 480;
+            //chart.Margin = new Thickness(100, 5, 10, 5);
+            //是否启用打印和保持图片
+            chart.ToolBarEnabled = false;
+
+            //设置图标的属性
+            chart.ScrollingEnabled = false;//是否启用或禁用滚动
+            chart.View3D = true;//3D效果显示
+
+            //创建一个标题的对象
+            Title title = new Title();
+
+            //设置标题的名称
+            title.Text = Name;
+            title.Padding = new Thickness(0, 10, 5, 0);
+
+            //向图标添加标题
+            chart.Titles.Add(title);
+
+            Axis yAxis = new Axis();
+            //设置图标中Y轴的最小值永远为0           
+            yAxis.AxisMinimum = 0;
+            //设置图表中Y轴的后缀          
+            yAxis.Suffix = suffix;
+            chart.AxesY.Add(yAxis);
+
+            foreach (var kvp in dict)
+            {
+                // 创建一个新的数据线。               
+                DataSeries dataSeries = new DataSeries();
+
+                // 设置数据线的格式
+                dataSeries.RenderAs = RenderAs.Column;//柱状Stacked
+                dataSeries.LegendText = kvp.Key;
+                // 设置数据点              
+                foreach (var subkvp in kvp.Value)
+                {
+                    // 创建一个数据点的实例。                   
+                    DataPoint dataPoint = new DataPoint();
+
+                    // 设置X轴点                    
+                    dataPoint.AxisXLabel = subkvp.Key;
+                        //设置Y轴点                   
+                        dataPoint.YValue = subkvp.Value;
+
+                        //添加一个点击事件        
+                        //dataPoint.MouseLeftButtonDown += new MouseButtonEventHandler(dataPoint_MouseLeftButtonDown);
+                        //添加数据点                   
+                        dataSeries.DataPoints.Add(dataPoint);
+                }
+                // 添加数据线到数据序列。                
+                chart.Series.Add(dataSeries);
+                //将生产的图表增加到Grid，然后通过Grid添加到上层Grid.           
+            }
+            g_chart.Children.Clear();
+            g_chart.Children.Add(chart);
+        }
         public void CreateChartColumn(string name, List<string> valuex, List<double> valuey, string suffix)
         {
             //创建一个图标
@@ -196,9 +293,8 @@ namespace ZC.Client
             DataSeries dataSeries = new DataSeries();
 
             // 设置数据线的格式
-            dataSeries.RenderAs = RenderAs.StackedColumn;//柱状Stacked
-
-
+            dataSeries.RenderAs = RenderAs.Column;//柱状Stacked
+            dataSeries.LegendText = "合格率";
             // 设置数据点              
             DataPoint dataPoint;
             for (int i = 0; i < valuex.Count; i++)
@@ -220,9 +316,40 @@ namespace ZC.Client
             // 添加数据线到数据序列。                
             chart.Series.Add(dataSeries);
 
+            DataSeries dataSeries2 = new DataSeries();
+
+            // 设置数据线的格式
+            dataSeries2.RenderAs = RenderAs.Column;//柱状Stacked
+            dataSeries2.LegendText = "不合格率";
+
+            // 设置数据点              
+            DataPoint dataPoint2;
+            for (int i = 0; i < valuex.Count; i++)
+            {
+                // 创建一个数据点的实例。                   
+                dataPoint2 = new DataPoint();
+
+                // 设置X轴点                    
+                dataPoint2.AxisXLabel = valuex[i];
+                //设置Y轴点                   
+                dataPoint2.YValue = 50;
+                
+                //添加数据点                   
+                dataSeries2.DataPoints.Add(dataPoint2);
+            }
+
+            // 添加数据线到数据序列。                
+            chart.Series.Add(dataSeries2);
             //将生产的图表增加到Grid，然后通过Grid添加到上层Grid.           
-            
+
             g_chart.Children.Add(chart);
+        }
+        
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+            Environment.Exit(0);
         }
     }
 }
